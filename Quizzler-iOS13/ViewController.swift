65bc5498cbba51ab5ae40f7400b29ac81aad22e7
River_Pease <riverpease15@gmail.com>
2024-10-02 12:26:12 -0400

    First version, no output or progress view, basic logic implemented

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,9 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    let quiz = [
+        ["Estrogen is primarily responsible for the development of female secondary sexual characteristics.", "True"],
+        ["Hormonal birth control methods eliminate all hormonal fluctuations in women.", "False"],
+        ["Testosterone is present in women and plays important roles, including influencing libido and bone density.", "True"],
+        ["Menopause marks the end of a woman's reproductive years and is characterized by a significant drop in estrogen levels.", "True"]
+    ]
+    
+    var qNum = 0
+    
+    @IBAction func answerBtnPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle // "True" or "False"
+        let answer = quiz[qNum][1] // Access current correct answer
+        
+        if userAnswer == answer {
+            print("Right")
+        } else {
+            print("Wrong")
+        }
+        
+        if qNum + 1 < quiz.count {
+            qNum += 1
+        } else {
+            qNum = 0
+        }
+        
+        updateUI()
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        
+        questionLabel.text = quiz[qNum][0]
+        
+        
     }
 
 
